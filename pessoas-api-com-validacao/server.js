99daeb207e3342bc127279a0e939fa859a39fcf7
Oriana0615 <oriana25salazar@gmail.com>
2024-04-16 22:48:23 -0300

    Crud com validacoes

---
+++ pessoas-api-com-validacao/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+
+
+let pessoas = [
+    { id: 1, nome: "João", idade: 20, email: "joao@email.com", telefone: "61900010002" }
+];
+
+
+app.get('/pessoas', (req, res) => {
+    res.status(200).json(pessoas);
+});
+
+// Rota para recuperar uma pessoa específica pelo ID
+app.get('/pessoas/:id', (req, res) => {
+    const pessoa = pessoas.find(p => p.id === parseInt(req.params.id));
+    if (!pessoa) {
+        return res.status(404).send('Pessoa não encontrada');
+    }
+    res.status(200).json(pessoa);
+});
+
+// Rota para adicionar uma nova pessoa
+app.post('/pessoas', (req, res) => {
+    if (!req.body.nome || !req.body.idade || !req.body.email || !req.body.telefone) {
+        return res.status(400).send('Todos os atributos devem ser preenchidos');
+    }
+    const novaPessoa = {
+        id: pessoas.length + 1, 
+        nome: req.body.nome,
+        idade: req.body.idade,
+        email: req.body.email,
+        telefone: req.body.telefone
+    };
+    pessoas.push(novaPessoa);
+    res.status(201).json(novaPessoa);
+});
+
+// Rota para atualizar uma pessoa existente pelo ID
+app.put('/pessoas/:id', (req, res) => {
+    const pessoa = pessoas.find(p => p.id === parseInt(req.params.id));
+    if (!pessoa) {
+        return res.status(404).send('Pessoa não encontrada');
+    }
+    if (!req.body.nome || !req.body.idade || !req.body.email || !req.body.telefone) {
+        return res.status(400).send('Todos os atributos devem ser preenchidos');
+    }
+    pessoa.nome = req.body.nome;
+    pessoa.idade = req.body.idade;
+    pessoa.email = req.body.email;
+    pessoa.telefone = req.body.telefone;
+    res.status(200).json(pessoa);
+});
+
+// Rota para remover uma pessoa pelo ID
+app.delete('/pessoas/:id', (req, res) => {
+    const index = pessoas.findIndex(p => p.id === parseInt(req.params.id));
+    if (index === -1) {
+        return res.status(404).send('Pessoa não encontrada');
+    }
+    pessoas.splice(index, 1);
+    res.status(204).send(); // Nenhum conteúdo, mas sucesso
+});
+
+// Inicializa o servidor para ouvir na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
